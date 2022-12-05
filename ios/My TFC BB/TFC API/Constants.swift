94054f47132cc230799374f908f5d9c278d9922c
Kyle Satti <k@kylejs.dev>
2022-12-04 22:27:58 -0500

    Set up Xcode Cloud to build protos

--- ios/My TFC BB/TFC API/Constants.swift
+++ ios/My TFC BB/TFC API/Constants.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-//let TFC_API_HOST = "my-tfc.up.railway.app"
-//let TFC_API_PORT = 443
+#if LOCALHOST_API
+let TFC_API_HOST = "my-tfc.up.railway.app"
+let TFC_API_PORT = 443
+#elseif PRODUCTION_API
 let TFC_API_HOST = "0.0.0.0"
 let TFC_API_PORT = 3000
+#endif
