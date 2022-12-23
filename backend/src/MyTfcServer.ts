5e7e5cfc630e144c90d8f1cc5845c6fde1264d97
Kyle Satti <k@kylejs.dev>
2022-12-23 15:25:35 -0500

    Fix imports

--- backend/src/MyTfcServer.ts
+++ backend/src/MyTfcServer.ts
@@ -7,8 +7,8 @@ import { sendTestPushNoticationHandler } from "./handlers/sendTestPushNotication
 import { updatePushTokenHandler } from "./handlers/updatePushToken";
 import { Server, ServerCredentials } from "@grpc/grpc-js";
 import { MyTfcService } from "./generated/proto/my_tfc_bb/v1/my_tfc_bb";
-import { getDevicesHandler } from "handlers/getDevices";
-import { removeDeviceHandler } from "handlers/removeDeviceHandler";
+import { getDevicesHandler } from "./handlers/getDevices";
+import { removeDeviceHandler } from "./handlers/removeDeviceHandler";
 
 export class MyTfcServiceImpl implements MyTfcServer {
   [method: string]: UntypedHandleCall;
