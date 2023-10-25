b5600cb7c19ede961e4a3e5fd415467e7b25f82d
Kyle Satti <k@kylejs.dev>
2023-10-25 00:00:04 -0400

    Handle migration from old keychain to new

--- ios/tfc-bb-core/KeychainManager.swift
+++ ios/tfc-bb-core/KeychainManager.swift
@@ -9,10 +9,16 @@ import Foundation
 import KeychainAccess
 
 public struct KeychainManager {
-    private static let BACKEND_DEVICE_ID_KEY = "backend_assigned_device_id"
+    private static let OLD_BACKEND_DEVICE_ID_KEY = "backend_assigned_device_id"
+    private static let BACKEND_DEVICE_ID_KEY = "backend_assigned_device_id_shared"
+    private static let oldKeychain = Keychain()
     private static let keychain = Keychain(service: "dev.kylejs.My-TFC-BB", accessGroup: "X2TBSUCASC.dev.kylejs.My-TFC-BB.shared")
 
     public static func getBackendAssignedId() -> String? {
+        if let oldStoredId = try? oldKeychain.getString(OLD_BACKEND_DEVICE_ID_KEY) {
+            setBackendAssignedId(oldStoredId)
+            try? oldKeychain.remove(OLD_BACKEND_DEVICE_ID_KEY)
+        }
         return try? keychain.getString(BACKEND_DEVICE_ID_KEY)
     }
 
