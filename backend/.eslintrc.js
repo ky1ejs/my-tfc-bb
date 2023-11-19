e0638fe86a652eaf39a74afa662799da395ea644
Kyle Satti <k@kylejs.dev>
2023-11-18 22:44:15 -0500

    Linter

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": false,
-        "es2021": true
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "rules": {
-      "prettier/prettier": "error"
-    }
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageDirectory: 'test_output/jest_coverage'
+  preset: "ts-jest",
+  testEnvironment: "node",
+  coverageDirectory: "test_output/jest_coverage",
 };
